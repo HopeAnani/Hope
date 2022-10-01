fccf2870e86f4aecdd84e0d155d466a6012725cf
Anani Samuel <94631710+HopeAnani@users.noreply.github.com>
2022-10-01 15:36:41 +0300

    Added opacity and fixed some features

--- Image Editing app/js/main.js
+++ Image Editing app/js/main.js
@@ -1,20 +1,21 @@
-let upload_img_box = document.querySelector(".upload_img_box");
-let selectedImage = document.querySelector("#selectedImage");
-let choose_image = document.querySelector(".choose_image");
+let upload_img_box = document.querySelector('.upload_img_box');
+let selectedImage = document.querySelector('#selectedImage');
+let choose_image = document.querySelector('.choose_image');
 
-let image_holder = document.querySelector(".image_holder");
-let image = document.querySelector("#image");
+let image_holder = document.querySelector('.image_holder');
+let image = document.querySelector('#image');
 
-let slider = document.querySelectorAll(".slider");
-let show_value = document.querySelectorAll(".show_value");
+let slider = document.querySelectorAll('.slider');
+let show_value = document.querySelectorAll('.show_value');
 
-let list_options = document.querySelectorAll("ul li");
+let list_options = document.querySelectorAll('ul li');
 
-let options = document.querySelector(".options");
-let option = document.querySelectorAll(".option");
+let options = document.querySelector('.options');
+let option = document.querySelectorAll('.option');
 
-let clearAll = document.querySelector("#clearAll");
-let remove_img_btn = document.querySelector("#remove_img_btn");
+let clearAll = document.querySelector('#clearAll');
+let remove_img_btn = document.querySelector('#remove_img_btn');
+
 let rotateImage = document.querySelector('#image');
 let rightBtn = document.querySelector('#right');
 let leftBtn = document.querySelector('#left');
@@ -34,45 +35,52 @@ let rotateDeg = 0;
 
 // let rotateOptions = document.querySelectorAll(".rotate");
 
-let canvas = document.querySelector("#image_canvas");
-const context = canvas.getContext("2d");
+
+let canvas = document.querySelector('#image_canvas');
+const context = canvas.getContext('2d');
 
 let File_Name;
 let Edited = false;
 
+
+
 /*handle choose image event*/
 upload_img_box.addEventListener("click", function () {
-  selectedImage.click();
+   selectedImage.click();
 });
+
 
 /*choose image event*/
 selectedImage.addEventListener("change", function () {
-  const file = this.files[0];
-  if (file) {
-    const reader = new FileReader();
-    File_Name = file.name;
+   const file = this.files[0];
 
-    choose_image.style.display = "none";
-    image_holder.style.display = "block";
-    image_holder.style.backgroundColor = "transparent";
+   if (file) {
+      const reader = new FileReader();
+      File_Name = file.name;
 
-    reader.addEventListener("load", function () {
-      image.setAttribute("src", this.result);
-      originalImage = image.getAttribute("src");
-    });
+      choose_image.style.display = "none";
+      image_holder.style.display = "block";
+      image_holder.style.backgroundColor = "transparent";
 
-    reader.readAsDataURL(file);
-    remove_img_btn.style.display = "block";
-  }
+      reader.addEventListener("load", function () {
+         image.setAttribute("src", this.result);
+         originalImage = image.getAttribute("src");
+      });
 
-  if (Edited == false) {
-    Edited = true;
-  }
-});
+      reader.readAsDataURL(file);
+      remove_img_btn.style.display = "block";
+   }
+
+   if (Edited == false) {
+      Edited = true;
+   }
+
+})
+
 
 /*function call when slider value change*/
 for (let i = 0; i <= slider.length - 1; i++) {
-  slider[i].addEventListener("input", editImage);
+   slider[i].addEventListener('input', editImage);
 }
 
 function editImage() {
@@ -83,6 +91,7 @@ function editImage() {
    let hue = document.querySelector('#hue');
    let saturation = document.querySelector('#saturation');
    let invertion = document.querySelector('#invertion');
+   let opacity = document.querySelector('#opacity');
 
 
    let brightValShow = document.querySelector('#brightVal');
@@ -92,6 +101,7 @@ function editImage() {
    let hueValShow = document.querySelector('#hueVal');
    let saturationValShow = document.querySelector('#saturationVal');
    let invertionValShow = document.querySelector('#invertionVal');
+   let opacityValShow = document.querySelector('#opacityVal');
 
    let brightVal = bright.value;
    let contrastVal = contrast.value;
@@ -100,6 +110,7 @@ function editImage() {
    let hueVal = hue.value;
    let satuVal = saturation.value;
    let invertVal = invertion.value;
+   let opacityVal = opacity.value;
 
    brightValShow.innerHTML = brightVal;
    contrastValShow.innerHTML = contrastVal;
@@ -108,36 +119,24 @@ function editImage() {
    hueValShow.innerHTML = hueVal;
    saturationValShow.innerHTML = satuVal;
    invertionValShow.innerHTML = invertVal;
+   opacityValShow.innerHTML = opacityVal;
 
-   image.style.filter = 'grayscale(' + greyVal + '%) invert('+ invertVal + '%) hue-rotate(' + hueVal + 'deg) brightness(' + brightVal + '%) blur(' + blurVal + 'px) contrast(' + contrastVal + '%) saturate(' + satuVal + ')';
+   image.style.filter = 'grayscale(' + greyVal + '%) invert('+ invertVal + '%) opacity(' + opacityVal + '%) hue-rotate(' + hueVal + 'deg) brightness(' + brightVal + '%) blur(' + blurVal + 'px) contrast(' + contrastVal + '%) saturate(' + satuVal + ')';
 
-   context.filter = 'grayscale(' + greyVal + '%) invert('+ invertVal + '%) hue-rotate(' + hueVal + 'deg) brightness(' + brightVal + '%) blur(' + blurVal + 'px) contrast(' + contrastVal + '%) saturate(' + satuVal + ')';
+   context.filter = 'grayscale(' + greyVal + '%) invert('+ invertVal + '%) opacity(' + opacityVal + '%) hue-rotate(' + hueVal + 'deg) brightness(' + brightVal + '%) blur(' + blurVal + 'px) contrast(' + contrastVal + '%) saturate(' + satuVal + ')';
    
    clearAll.style.transform = 'translateY(0px)';
    clearAll.style.display = 'flex';
 }
 
+
 /*handle each option click even*/
 list_options.forEach((list_option, index) => {
-  list_option.addEventListener("click", function () {
-    if (image.getAttribute("src") == "") {
-      alert("Choose Image First");
-    } else {
-      options.style.transform = "translateY(0px)";
+   list_option.addEventListener('click', function () {
 
-      if (Edited == true) {
-        canvas.height = image.naturalHeight;
-        canvas.width = image.naturalWidth;
 
-        for (let i = 0; i <= 6; i++) {
-          if (index != i) {
-            list_options[i].classList.remove("active_option");
-            option[i].classList.remove("active_controller");
-          } else {
-            this.classList.add("active_option");
-            option[i].classList.add("active_controller");
-          }
-        }
+      if (image.getAttribute('src') == "") {
+         alert("Choose Image First");
       } else {
 
          options.style.transform = 'translateY(0px)';
@@ -146,7 +145,7 @@ list_options.forEach((list_option, index) => {
             canvas.height = image.naturalHeight;
             canvas.width = image.naturalWidth;
 
-            for (let i = 0; i <= 6; i++) {
+            for (let i = 0; i <= 7; i++) {
 
                if (index != i) {
                   list_options[i].classList.remove("active_option");
@@ -163,65 +162,71 @@ list_options.forEach((list_option, index) => {
          }
 
       }
-    }
-  });
-});
+
+   })
+})
+
 
 /*download image btn click*/
 function Download_btn() {
-  if (image.getAttribute("src") != "") {
-    if (Edited == true) {
-      context.drawImage(image, 0, 0, canvas.width, canvas.height);
-      var jpegUrl = canvas.toDataURL("image/jpg");
 
-      const link = document.createElement("a");
-      document.body.appendChild(link);
+   if (image.getAttribute('src') != "") {
 
-      link.setAttribute("href", jpegUrl);
-      link.setAttribute("download", File_Name);
-      link.click();
-      document.body.removeChild(link);
-    }
-  }
+      if (Edited == true) {
+         context.drawImage(image, 0, 0, canvas.width, canvas.height);
+         var jpegUrl = canvas.toDataURL("image/jpg");
+
+         const link = document.createElement("a");
+         document.body.appendChild(link);
+
+         link.setAttribute("href", jpegUrl);
+         link.setAttribute("download", File_Name);
+         link.click();
+         document.body.removeChild(link);
+
+      }
+   }
 }
+
 
 /*clear or reset range value*/
 clearAll.addEventListener("click", function () {
-  clearAllRangeValue();
-});
+   clearAllRangeValue();
+})
 
 function clearAllRangeValue() {
-  image.style.filter = "none";
-  context.filter = "none";
-  for (let i = 0; i <= slider.length - 1; i++) {
-    if (i == 0) {
-      slider[i].value = "100";
-    } else if (i == 1) {
-      slider[i].value = "100";
-    } else {
-      slider[i].value = "0";
-    }
-  }
-  image.src = originalImage;
+   image.style.filter = 'none';
+   context.filter = 'none';
 
-  editImage();
-  clearAll.style.transform = "translateY(150px)";
-  clearAll.style.display = 'flex';
-  crop.style.transform = "translateY(150px)";
+   for (let i = 0; i <= slider.length - 1; i++) {
+      if (i == 0) {
+         slider[i].value = '100';
+      } else if(i == 1) {
+         slider[i].value = '100';
+      }else{
+         slider[i].value = '0';
+      }
+   }
+
+   editImage();
+   clearAll.style.transform = 'translateY(150px)';
+   clearAll.style.display = 'flex';
+   crop.style.transform = "translateY(150px)";
 }
 
 /*remove image btn click*/
 remove_img_btn.addEventListener("click", function () {
-  image.src = "";
-  this.style.display = "none";
-  choose_image.style.display = "block";
-  image_holder.style.display = "none";
-  options.style.transform = "translateY(80px)";
-  clearAllRangeValue();
-  selectedImage.value = null;
-});
+   image.src = "";
+   this.style.display = "none";
+   choose_image.style.display = "block";
+   image_holder.style.display = "none";
+   options.style.transform = 'translateY(80px)';
+   clearAllRangeValue();
+   selectedImage.value = null;
+})
 
 /*Rotate image */
+   
 let rotateDeg2 = 0, rotateDeg3 = 0;
 rightBtn.addEventListener("click", () => {
    console.log(rotateDeg);
@@ -248,25 +253,24 @@ flipUp.addEventListener("click", () => {
   rotateImage.style.transform = `rotateZ(${rotateDeg}deg)`; //"rotateZ("+ rotateDeg3 +"deg)"
 });
 
+   zoomIn.addEventListener('click',() => {
+      var currWidth = zoomImage.clientWidth;
+      if(currWidth == 5000){
+         alert('Maximum Zoom In reached');
+      }
+      else{
+         zoomImage.style.width = (currWidth + 40) + 'px';
+      }
+   })
 
-zoomIn.addEventListener('click',() => {
-   var currWidth = zoomImage.clientWidth;
-   if(currWidth == 5000){
-      alert('Maximum Zoom In reached');
-   }
-   else{
-      zoomImage.style.width = (currWidth + 40) + 'px';
-   }
-})
-
-zoomOut.addEventListener('click',() => {
-   var currWidth = zoomImage.clientWidth;
-   if(currWidth == 40){
-      alert('Maximum zoom out reached');
-   }else{
-      zoomImage.style.width  = (currWidth - 40) + 'px';
-   }
-})
+   zoomOut.addEventListener('click',() => {
+      var currWidth = zoomImage.clientWidth;
+      if(currWidth == 40){
+         alert('Maximum zoom out reached');
+      }else{
+         zoomImage.style.width  = (currWidth - 40) + 'px';
+      }
+   })
 
 //enable disable cropping tool
 let cropper = null;
@@ -302,4 +306,7 @@ document.getElementById('en_de_crop').addEventListener('click', ()=>{
       isEnabled = false;
    }
 });
+
+
+/*///////////////////////////////////////*/ 
 
